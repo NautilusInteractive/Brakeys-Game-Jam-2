0021ea290177e94d7735f0cb1e880ba0ce851d8c
Adam Fowler <ajohnfowler@gmail.com>
2019-02-17 18:52:02 -0400

    Added a new dog controller

--- Guide Dog/Assets/Scripts/Dog_Controller.cs
+++ Guide Dog/Assets/Scripts/Dog_Controller.cs
@@ -5,14 +5,38 @@ using UnityEngine;
 public class Dog_Controller : MonoBehaviour
 {
     [SerializeField]
-    private float RotateSpeed = 100.0f;
+    private float speed = 2.0f;
     [SerializeField]
-    private float MoveSpeed = 2.0f;
-    
+    private float turnSpeed = 4.0f;
+
+    private Vector3 direction;
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0.0f, Input.GetAxis("Horizontal"), 0.0f) * Time.deltaTime * RotateSpeed);
-        transform.Translate(new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical")) * Time.deltaTime * MoveSpeed);
+        Move();
+
+        if (Input.GetKeyDown("space")) {
+            Bark();
+        }
+        if (Input.GetKeyDown("e")) {
+            Interact();
+        }
+    }
+
+    private void Move() {
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        transform.Translate(direction * Time.deltaTime * speed, Space.World);
+
+        Vector3 lookDirection = Vector3.RotateTowards(transform.forward, direction, turnSpeed * Time.deltaTime, 0.0f);
+        // Move our position a step closer to the target.
+        transform.rotation = Quaternion.LookRotation(lookDirection);
+    }
+
+    private void Bark() {
+        Debug.Log("Bark");
+    }
+
+    private void Interact() {
+        Debug.Log("Interact");
     }
 }
--- Guide Dog/Assets/Scripts/Person_Controller.cs
+++ Guide Dog/Assets/Scripts/Person_Controller.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Person_Controller : MonoBehaviour
 {
+    [SerializeField]
+    private Vector3 direction;
+    [SerializeField]
+    private bool move;
+    [SerializeField]
+    private float speed = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = transform.forward;
+        move = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (move) {
+            transform.Translate(direction * Time.deltaTime * speed);
+        }
     }
 }
